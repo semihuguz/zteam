66c590eb0451d522a2ed5d2fc59de5c163ad4cc9
Semih <rusnqel70@gmail.com>
2022-06-28 22:41:55 +0300

    First commit of project

---
+++ src/main/java/com/vana/zteam/entity/User.java
@@ -0,0 +1,44 @@
+package com.vana.zteam.entity;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int userId;
+
+    @NotNull
+    @Column(name="user_name")
+    private String userName;
+
+    @NotNull
+    @Column(name="mail")
+    private String mail;
+
+    @NotNull
+    @Column(name="password")
+    private String password;
+
+    @NotNull
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="crate_date")
+    private LocalDateTime releaseDate;
+
+
+
+}
---
+++ src/test/java/com/vana/zteam/ZteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vana.zteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
